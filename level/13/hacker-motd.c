4d16eee2d7d804aaf9d16352c09e3b43952e37de
malwaredetective <malwaredetetive@outlook.com>
2025-06-08 14:35:25 -0500

    updated base/legacy image in levels

--- level/13/hacker-motd.c
+++ level/13/hacker-motd.c
@@ -3,11 +3,11 @@
 #include <time.h>
 
 const char* quotes[] = {
-    "Hacking is not a crime. It\'s a skill set.",
-    "The quieter you become, the more you can hear.",
-    "There is no patch for human stupidity.",
-    "I hack, therefore I am.",
-    "Knowledge, like air, is vital to life. Like air, no one should be denied it."
+    "A true hacker sees failure not as defeat, but as data.",
+    "The quieter you become, the more you are able to hear.",
+    "The world isn’t run by weapons anymore, or energy, or money. It’s run by little ones and zeroes, little bits of data.",
+    "The best defense is a good offense.",
+    "If you can't find a way, create one."
 };
 
 int main() {
